1e2924a3e05d30605eff0d1f17258a337ec08c51
Lewis <lewis@tya.email>
2017-11-16 15:55:40 +0800

    add Uilib & Utils

---
+++ UiLIb.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+"""
+File  : UiLIb.py
+Author: Lewis Lau
+Date  : 2017/11/16
+Desc  : 
+"""
+
+# if __name__ == "__main__":
+#    print ('Hello world"')
+
+
+# coding=utf-8
+fmt = '\033[0;3{}m{}\033[0m'.format
+
+BLACK = 0  # 黑
+RED = 1  # 红
+GREEN = 2  # 绿
+YELLOW = 3  # 棕
+BLUE = 4  # 蓝
+PURPLE = 5  # 紫
+CYAN = 6  # 青
+GRAY = 7  # 灰
+
+'''
+print fmt(color.BLACK, 'kzc')
+print fmt(color.RED, 'kzc')
+print fmt(color.GREEN, 'kzc')
+print fmt(color.YELLOW, 'kzc')
+print fmt(color.BLUE, 'kzc')
+print fmt(color.PURPLE, 'kzc')
+print fmt(color.CYAN, 'kzc')
+print fmt(color.GRAY, 'kzc')
+'''
---
+++ Utils.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+"""
+File  : Utils.py.py
+Author: Lewis Lau
+Date  : 2017/11/16
+Desc  : 
+"""
+
+# if __name__ == "__main__":
+#    print ('Hello world"')
+    
