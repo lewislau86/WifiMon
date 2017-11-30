8e3af15dd84735b3b785404bc4a6f36f2199d91d
Lewis <lewis@tya.email>
2017-11-30 17:33:09 +0800

    start sniff

--- PacketParse.py
+++ PacketParse.py
@@ -153,7 +153,7 @@ class packetParse(object):
                 "utf-8") + W + ']'
             self.__Numap += 1
 
-        logger.info(self.__args.delimiter.join(fields))
+       print(self.__args.delimiter.join(fields))
 
     def do_sniff(self , intf):
         self.__intf = intf
