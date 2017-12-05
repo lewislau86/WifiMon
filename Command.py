dec45f776dceb5d12397595979ea6fcb84ab0b3c
Lewis <lewis@tya.email>
2017-12-05 11:10:21 +0800

    show hidden ssid

--- Command.py
+++ Command.py
@@ -33,9 +33,9 @@ class Command(object):
         if self.__status.verboseMode == self.__status.getCurrentMode():
             self.__status.setMode(self.__status.cmdMode)
             Verbase.setSilent(True)
-        elif Status.cmdMode == Status.getCurrentMode():
+        #elif Status.cmdMode == Status.getCurrentMode():
             # 如果已经在命令模式，仍然Ctrl+, 则退出
-            sys.exit(0)
+        #    sys.exit(0)
         else:
             UiLIb.CPrint.YELLOW("Uninitialized!\r\n")
 
