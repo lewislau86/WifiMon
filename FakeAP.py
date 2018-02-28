b7d8dcba85997bc2986384602cf1cc714b38824e
Lewis <lewis@tya.email>
2018-02-28 16:10:25 +0800

    Log and write to csv

---
+++ FakeAP.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+"""
+File  : FakeAP.py
+Author: Lewis Lau
+Date  : 2017/12/27
+Desc  : 
+"""
+
+class FakeAP(object):
+    def generateCfgFile(self):
+        pass
